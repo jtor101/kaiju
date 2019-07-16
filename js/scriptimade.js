cd480893e3146a7fe8f057ee1d0fdb47bdf71ec5
John Torres <jtorres0802@gmail.com>
2019-07-16 16:55:51 -0400

    Added folder for JS.

---
+++ js/scriptimade.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#submit').on('click', function() {
+        var st = '';
+        $('#kborderinfo input[type=text],input[type=text],input[type=email],input[type=number],select,select').each(function() {
+            st = st + '<td>' + $(this).val() + '</td>';
+            $(this).val('');
+        });
+        $('#details').append('<tr>' + st + '</tr>');
+        return false;
+    });
+});
